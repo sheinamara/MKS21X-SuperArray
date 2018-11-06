a899e052bb47d23280411aca4cb11aa509d75d33
sheinamara <ashein00@stuy.edu>
2018-11-06 14:06:27 -0500

    fixing code! got tips from peers

--- SuperArray.java
+++ SuperArray.java
@@ -1,3 +1,7 @@
+// TIPS GAINED FROM MY PEERS ABOUT FIXING MY CODE:
+//  1. try indicating "this" versus "other" to make sure you understand things better
+//  2. read over directions over and over again in order to make sure you know what's going on
+//  3. add throws; you didn't have any...
 public class SuperArray{
 // PHASE ONE
   // private fields so we don't have random things accessing it!
@@ -21,6 +25,10 @@ public class SuperArray{
   // 2
   public int size(){
     int count = 0;
+    if (this.data.length == 0){
+      this.size = count;
+      return this.size();
+    }
     for (int i = 0; i < this.data.length; i++){ // needed actual code lol
       if (data[i] != null){
         count = count + 1;
@@ -30,25 +38,29 @@ public class SuperArray{
     return this.size;
   }
   public boolean isEmpty(){
-    return size == 0;
+    return this.size() == 0;
   }
 
   // 3 SANITY CHECK DONE
   public boolean add(String input){
-    if (size == data.length){
-      resize(); // added a later private resizing method from phase 2
-      // return false;
+    if (this.size() == this.data.length){
+      this.resize(); // added a later private resizing method from phase 2
+      this.data[this.size()] = input;
+      return true;
     }
-    // else{
-    size ++; // i think this switch will debug it ???
-    data[size] = input;
-    return true;
-    // }
+    if (this.size() < this.data.length){
+      this.data[this.size()] = input;
+      return true;
+    }
+    return false;
   }
 
   // 4
   public String toString(){
     // we cannot just say return "" + data or it will return the address
+    if (this.size() == 0){
+      return "[]";
+    }
     String theRegularArray = "["; // the beginning of the array we need to print
     for (int i = 0; i < size() - 1; i++){ // WE USE SIZE HERE
       theRegularArray = theRegularArray + data[i] + ", "; // need to add the comma to separate the strings
@@ -61,6 +73,9 @@ public class SuperArray{
   }
   public String toStringDebug(){
     // we cannot just say return "" + data or it will return the address
+    if (this.size() == 0){
+      return "[]";
+    }
     String theAmazingArray = "["; // the beginning of the array we need to print
     for (int i = 0; i < data.length - 1; i++){ // WE USE BUILT-IN METHOD TO GET THE NULLS
       theAmazingArray = theAmazingArray + data[i] + ", "; // need to add the comma to separate the strings
@@ -71,19 +86,19 @@ public class SuperArray{
 
   // 5
   public String get(int index){
-    if (index < 0 || index >= size()){
-      return null;
+    if (index < 0 || index >= this.size()){
+      throw new IndexOutOfBoundsException("Check the index! Is this valid?");
     }
-    return "" + data[index];
+    return "" + this.data[index];
   }
 
   // 6
   public String set(int index, String desiredValue){ // replaces the string so don't worry about shifting
     if (index < 0 || index >= size()){
-      return null;
+      throw new IndexOutOfBoundsException("Check the index! Is this valid?");
     }
-    String old = "" + data[index];
-    data[index] = desiredValue;
+    String old = "" + this.data[index];
+    this.data[index] = desiredValue;
     return old;
   }
 
